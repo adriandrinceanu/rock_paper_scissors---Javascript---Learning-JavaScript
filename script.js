3a9c4f8e68220f7e809c1fac728ec0c3170f4330
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-02-13 15:42:16 +0200

    added scoreboard and score reset

--- script.js
+++ script.js
@@ -52,54 +52,62 @@ function playRound(playerSelection, computerSelection) {
   content.appendChild(scissors);
 
   const buttons = document.querySelectorAll('button');
+  const scoreboard = document.querySelector('#score');
 
-  buttons.forEach((button) =>{
+  scoreboard.setAttribute('class', 'score');
 
-    button.addEventListener('click', ()=>{
-        let computerSelection = getComputerChoice();
-        let winner = playRound(button.value, computerSelection);
-        let para = document.createElement('p');
-        para.textContent = winner;
-        content.appendChild(para); 
-    });
 
-  });
+  let computer_winnings = 0;
+  let player_winnings = 0;
+  let draws = 0;
+  let para = document.createElement('p');
+  content.appendChild(para);
 
 
 
-
-
- 
-//   function playGame() {
-//     let computer_winnings = 0;
-//     let player_winnings = 0;
-//     let draws = 0;
-
-//     for (let i = 0; i < 5; i++) {
-
-//         let playerSelection = getPlayerChoice();
-//         let computerSelection = getComputerChoice();
-
-//         console.log('computer:', computerSelection);
-//         console.log('player:', playerSelection);
-
-//         let result = playRound(playerSelection, computerSelection);
+    buttons.forEach((button) => {
+        button.addEventListener('click', ()=>{
+            let computerSelection = getComputerChoice();
+            let winner = playRound(button.value, computerSelection);
+            para.textContent = winner;
         
-//         if (result === 'You win, paper beats rock.' 
-//         || result === 'You win, rock beats scissors' 
-//         || result === 'You win, scissors beat paper') {
-//             player_winnings += 1
-//         } else if (result === "it's a draw") {
-//             draws += 1
-//         } else {
-//             computer_winnings += 1
-//         }
-//         console.log('result:', result);
-//         }
-//         console.log(`Computer wins ${computer_winnings} times, player wins ${player_winnings} times and it was draw ${draws} times`);
-//     }
+            
+            if (winner === 'You win, paper beats rock.' 
+                    || winner === 'You win, rock beats scissors' 
+                    || winner === 'You win, scissors beat paper') {
+                        player_winnings += 1
+                    } else if (winner === "it's a draw") {
+                        draws += 1
+                    } else {
+                        computer_winnings += 1
+                    };
+            scoreboard.textContent = `Computer wins ${computer_winnings} times, player wins ${player_winnings} times and it was draw ${draws} times`;  
+            
+            if (player_winnings === 5) {
+                alert("Human player won!");
+                player_winnings = 0;
+                computer_winnings = 0;
+                draws = 0;
+              };
+              if (computer_winnings === 5) {
+                alert('Computer won! Find Sarah Conner!!!');
+                player_winnings = 0;
+                computer_winnings = 0;
+                draws = 0;
+              };
+              if (draws === 5) {
+                alert('Thight thight tight!');
+                player_winnings = 0;
+                computer_winnings = 0;
+                draws = 0;
+              };
+        });
+  });
+  
+  content.prepend(scoreboard);
 
 
-// console.log(playGame());
+
+  
 
 
